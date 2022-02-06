2110f4245a4af3373bbe76db85b78de50ada344d
Omar Valdivia Samudio <92412378+omarvalsam@users.noreply.github.com>
2022-02-05 19:19:17 -0700

    added model user and index

---
+++ models/User.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+
+class User extends Model {}
+
+//Defining our table collumns and configuration
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      dataType: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+
+    sequelize,
+
+    timestamps: false,
+    FreezeTableName: true,
+    underscored: true,
+    tableName: "user",
+    modelName: "user",
+  }
+);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const routes = require("./controllers");
 // const session = require("express-session");
 // const exphbs = require("express-handlebars");
 
