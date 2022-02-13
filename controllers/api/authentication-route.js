1430e498424f8f38ee3b2c016915fd2b4acca4d5
CherMill <cmille2016@gmail.com>
2022-02-12 17:12:50 -0700

    commented code sections

---
+++ controllers/api/authentication-route.js
@@ -0,0 +1,45 @@
+//middleware start
+const router = express();
+router.use(require('serve-static')(__dirname + '/../../public'));
+router.use(require('cookie-parser')());
+router.use(require('body-parser').urlencoded({ extended: true }));
+router.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+router.use(passport.initialize());
+router.use(passport.session());
+//middleware end
+
+//strategy start
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
+//strategy end
+
+//sessions start
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+// sessions end
+
+//authenticate request start
+router.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+//authenticate request end
+
+
+  //found at https://openbase.com/js/passport
