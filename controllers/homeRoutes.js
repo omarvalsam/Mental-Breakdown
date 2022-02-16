a41d2eeae305c8ab4de80caee58834834c5fc338
Omar Valdivia Samudio <92412378+omarvalsam@users.noreply.github.com>
2022-02-15 18:04:15 -0700

    fixed broken code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,24 +8,10 @@ router.get("/", (req, res) => {
 });
 
 // =================================================================
-// This code only will be used if we decide that
-<<<<<<< HEAD
-// we want a homepage that redirects us to the login page main login.html | redirects to login
-// router.get("/login", (req, res) => {
-//   if (req.session.loggedin) {
-//     res.redirect("/");
-//     return;
-//   }
-=======
 // we want a homepage that redirects us to the login page main login.html
 router.get("/login", (req, res) => {
   res.render("login");
-  // if (req.session.loggedin) {
-  //   res.redirect("/");
-  //   return;
-  // }
 });
->>>>>>> d9ddf38d99b9f7a1617d660dcfc06e8ac84c89d3
 
 // this route is intended to be used for the user interface, allowing for the tasks to be mapped out to user dashboard
 router.get("/userinterface", (req, res) => {
@@ -52,12 +38,11 @@ router.get("/userinterface", (req, res) => {
 // ======================         ======================
 // This code was written for single posts in mind,
 // will un comment when page is built with handle bars
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 // router.get("/Task/:id", (req, res) => {
 //   Task.findOne({
 //     where: { id: req.params.taskid },
-//     attributes: ["task_id", "title", "task_text", "created_at"], | redirects to Sara's two 
+//     attributes: ["task_id", "title", "task_text", "created_at"], | redirects to Sara's two
 //     include: [
 //       {
 //         model: User,
@@ -73,17 +58,11 @@ router.get("/userinterface", (req, res) => {
 //         return;
 //       }
 //       const task = dbtaskData.get({ plain: true });
-=======
-router.get("/Task/:id", (req, res) => {
+
+router.get("/:id", (req, res) => {
   Task.findOne({
     where: { id: req.params.taskid },
     attributes: ["task_id", "title", "task_text", "created_at"],
-=======
-router.get("/Task/:id", (req, res) => {
-  Task.findOne({
-    where: { id: req.params.taskid },
-    attributes: ["task_id", "title", "task_text", "created_at"], 
->>>>>>> cm-backend
     include: [
       {
         model: User,
@@ -98,16 +77,8 @@ router.get("/Task/:id", (req, res) => {
           .json({ message: "No Task found with this id, QUE LOSER" });
         return;
       }
-<<<<<<< HEAD
       const task = dbTaskData.get({ plain: true });
->>>>>>> d9ddf38d99b9f7a1617d660dcfc06e8ac84c89d3
-
       res.render("single-task", { task });
-=======
-      const task = dbtaskData.get({ plain: true });
-
-      res.render("single-task", { post });
->>>>>>> cm-backend
     })
     .catch((err) => {
       consoel.log(err);
